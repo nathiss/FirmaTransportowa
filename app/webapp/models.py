c7ff251f91289faa05adfe8bf88745f1a1f4a125
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2020-03-27 18:04:50 +0100

    Update webapp's models
    * Made migrations

--- app/webapp/models.py
+++ app/webapp/models.py
@@ -1,13 +1,17 @@
 import uuid
 
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, RegexValidator
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Vehicle(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    registration_number = models.CharField(max_length=20)
-    number_of_seats = models.IntegerField(validators=[MinValueValidator(1)])
+    registration_number = models.CharField(max_length=20, help_text='Max length: 20')
+    number_of_seats = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    model_fullname = models.CharField(max_length=100, blank=True, default='Unknown', help_text='Please enter the full name of the vehicle model.')
 
 
 class Person(models.Model):
@@ -17,16 +21,34 @@ class Person(models.Model):
         verbose_name_plural = 'People'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, help_text='Max length: 50')
+    last_name = models.CharField(max_length=50, help_text='Max length: 50')
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=(9), validators=[
+        RegexValidator(r'^[0-9]$', 'This is not a valid phone number.')
+    ])
+    pesel = models.CharField(max_length=(11), validators=[
+        RegexValidator(r'^[0-9]$', 'This is not a valid PESEL number.')
+    ])
 
 
 class Driver(Person):
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
 
 
+# https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
 class Client(Person):
-    email = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, *args, **kwargs):
+    if created:
+        # pylint: disable=no-member
+        Client.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, *args, **kwargs):
+    instance.client.save()
 
 
 class Place(models.Model):
@@ -40,7 +62,8 @@ class Connection(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     starting_place = models.ForeignKey(Place, related_name='FirstStop', on_delete=models.CASCADE)
     laststop_place = models.ForeignKey(Place, related_name='LastStop', on_delete=models.CASCADE)
-    price = models.IntegerField(validators=[MinValueValidator(0)])
+    distance = models.PositiveIntegerField(help_text='Please enter the distance in kilometers.')
+    price = models.PositiveIntegerField(validators=[MinValueValidator(1)], help_text='Please enter an integer value.')
     date_time = models.DateTimeField()
 
 
