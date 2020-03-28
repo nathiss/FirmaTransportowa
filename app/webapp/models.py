abf35fdbdb102be17f7ed25610c227b9909a6d31
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2020-03-28 19:17:32 +0100

    Add Post model

--- app/webapp/models.py
+++ app/webapp/models.py
@@ -92,3 +92,10 @@ class Ticket(models.Model):
 
     def __str__(self):
         return f'ticket-{self.id}'
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=100)
+    datetime = models.DateTimeField(auto_now_add=True, help_text='Creation date & time.')
+    content = models.TextField()
