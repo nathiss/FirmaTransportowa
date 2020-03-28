3c8cd1e7886546fc68cc1d8f5cd09743d5158136
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2020-03-28 12:39:36 +0100

    Create basic view structure

--- app/webapp/views.py
+++ app/webapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+
+class IndexView(View):
+  def get(self, request, *args, **kwargs):
+    return render(request, 'webapp/index.html')
