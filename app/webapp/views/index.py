65f142a4ca70071b4f9822ee7ee7b351cd2e090f
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2020-03-29 12:30:33 +0200

    Add post models render
    * Rename article.html to post.html

--- app/webapp/views/index.py
+++ app/webapp/views/index.py
@@ -1,7 +1,10 @@
 from django.views import View
 from django.shortcuts import render
 
+from ..models import Post
+
 
 class IndexView(View):
-  def get(self, request, *args, **kwargs):
-    return render(request, 'webapp/index.html')
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.order_by('-datetime')
+        return render(request, 'webapp/index.html', {"posts": posts})
