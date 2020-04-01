d8708c4167445d6baa78e95a7fe1794dbfbaaa94
Kuba Wielgus <Wielqus@gmail.com>
2020-04-01 21:28:49 +0200

    Basic register

--- app/webapp/views/register.py
+++ app/webapp/views/register.py
@@ -1,6 +1,8 @@
-from django.views           import View
-from django.shortcuts       import render, redirect
-from django.contrib.auth    import authenticate
+from django.views               import View
+from django.shortcuts           import render, redirect
+from django.contrib.auth        import authenticate
+from django.contrib.auth.models import User
+from django.core.exceptions     import ObjectDoesNotExist
 
 from ..forms import RegisterForm
 
@@ -12,5 +14,31 @@ class RegisterView(View):
         return render(request, 'webapp/register.html', {"form": form})
 
     def post(self, request, *args, **kwargs):
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['login']
+            password = form.cleaned_data['passwd']
+            repeatPassword = form.cleaned_data['repeatPasswd']
 
-        return render(request, 'webapp/register.html')
+            if (password != repeatPassword):
+                form.add_error(field=None, error='Hasła nie zgadzają się.')
+                return render(request, 'webapp/register.html', {"form": form})
+
+            user = self.search(username)
+            if user is True:
+                form.add_error(field=None, error='Użytkownik już istnieje.')
+                return render(request, 'webapp/register.html', {"form": form})
+
+            user = User.objects.create_user(username, None, password)
+
+            return render(request, 'webapp/register.html', {"form": form})
+
+        form.add_error(field=None, error='Formularz jest nieprawidłowy.')
+        return render(request, 'webapp/register.html', {"form": form})
+
+    def search(self, userstring):
+        try:
+            User.objects.get(username=userstring)
+            return True
+        except ObjectDoesNotExist:
+            return False
