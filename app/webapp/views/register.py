52a7d23f50518a0d19b0a1981d4541b44c20d607
Kuba Wielgus <Wielqus@gmail.com>
2020-04-02 07:02:18 +0200

    Finish register

--- app/webapp/views/register.py
+++ app/webapp/views/register.py
@@ -3,8 +3,9 @@ from django.shortcuts           import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth        import authenticate
 
-from ..forms import RegisterForm
-from ..forms import ClientForm
+from ..forms    import RegisterForm
+from ..forms    import ClientForm
+from ..models   import Client
 
 class RegisterView(View):
     def get(self, request, *args, **kwargs):
@@ -17,15 +18,21 @@ class RegisterView(View):
     def post(self, request, *args, **kwargs):
         form = RegisterForm(request.POST)
         client_form = ClientForm()
-        if form.is_valid():
+        if form.is_valid() and client_form.is_valid():
             username = form.cleaned_data['login']
             password = form.cleaned_data['passwd']
             email = form.cleaned_data['email']
 
-            User.objects.create_user(username, email, password)
+            first_name = client_form.cleaned_data['first_name']
+            last_name = client_form.cleaned_data['last_name']
+            phone_number = client_form.cleaned_data['phone_number']
+            pesel = client_form.cleaned_data['pesel']
+
+            user = User.objects.create_user(username, email, password)
+            Client.create(first_name=first_name, last_name=last_name, email=email, phone_number=phone_number, pesel=pesel, user=user)
+
             authenticate(username=username, password=password)
 
             return redirect('webapp:index')
             
         return render(request, 'webapp/register.html', {"form": form, "client_form": client_form})
-
