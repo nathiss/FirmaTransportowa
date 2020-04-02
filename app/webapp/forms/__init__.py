5407ba07ec868c3cf1bd702a20b3d496923d647e
Kuba Wielgus <Wielqus@gmail.com>
2020-04-02 06:49:58 +0200

    Add client form to register view

--- app/webapp/forms/__init__.py
+++ app/webapp/forms/__init__.py
@@ -1,2 +1,3 @@
 from .login     import LoginForm
 from .register  import RegisterForm
+from .client    import ClientForm
--- app/webapp/views/register.py
+++ app/webapp/views/register.py
@@ -4,16 +4,19 @@ from django.contrib.auth.models import User
 from django.contrib.auth        import authenticate
 
 from ..forms import RegisterForm
+from ..forms import ClientForm
 
 class RegisterView(View):
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('webapp:index')
         form = RegisterForm()
-        return render(request, 'webapp/register.html', {"form": form})
+        client_form = ClientForm()
+        return render(request, 'webapp/register.html', {"form": form, "client_form": client_form})
 
     def post(self, request, *args, **kwargs):
         form = RegisterForm(request.POST)
+        client_form = ClientForm()
         if form.is_valid():
             username = form.cleaned_data['login']
             password = form.cleaned_data['passwd']
@@ -24,5 +27,5 @@ class RegisterView(View):
 
             return redirect('webapp:index')
             
-        return render(request, 'webapp/register.html', {"form": form})
+        return render(request, 'webapp/register.html', {"form": form, "client_form": client_form})
 
