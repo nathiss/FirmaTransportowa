5648faa6b80086ca485b2ce842ffbb1e08bd206b
Kamil Rusin <kamil.jakub.rusin@gmail.com>
2020-05-14 18:10:30 +0200

    Add about_us & contect

--- app/webapp/views/__init__.py
+++ app/webapp/views/__init__.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render
+
 from .index import IndexView
 from .login import LoginView
 from .logout import LogoutView
@@ -7,3 +9,10 @@ from .post import CreatePostView
 from .post import DeletePostView
 from .post import EditPostView
 from .create_place import CreatePlaceView
+
+
+def about_us(request, *args, **kwargs):
+    return render(request, 'webapp/about_us.html')
+
+def contact(request, *args, **kwargs):
+    return render(request, 'webapp/contact.html')
